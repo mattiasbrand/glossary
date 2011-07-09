793cd79d8f64d78f08b34d47974a49ded9ee43d9
brand <brand@ubuntu.bredbandsbolaget.se>
2011-07-09 22:28:59 +0200

    Completed authorization with couchdb as user database

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var urlpaser = require('url');
 var jade = require('jade');
 var https = require('https');
+var cradle = require('cradle');
 
 var requiresLogin = function(req, res, next) {
         url = req.urlp = urlpaser.parse(req.url, true);
@@ -10,6 +11,7 @@ var requiresLogin = function(req, res, next) {
             return;
         }
 
+	console.log(req.session);
         if (req.session && req.session.user) {
             next();
         }
@@ -19,6 +21,15 @@ var requiresLogin = function(req, res, next) {
         }
     };
 
+// Init connection
+var connection = new(cradle.Connection)('https://brand.cloudant.com', 443, {
+    auth: { username: 'brand', password: 'underbar' }
+});
+
+// Init database
+var db = connection.database('glossary');
+
+// Init express
 app = express.createServer();
 
 app.configure(function() {
@@ -50,32 +61,25 @@ app.get('/login', function(req, res) {
 });
 
 app.post('/login', function(req, res) {
-    var auth = new Buffer('brand:underbar').toString('base64');
-    var options = {
-        host: 'brand.cloudant.com',
-        port: 443,
-        path: '/_all_dbs',
-        method: 'GET',
-        headers: {'Authorization' : 'Basic ' + auth }
-    };
-    
-    var request = https.request(options, function(res) {
-        console.log("statusCode: ", res.statusCode);
-        console.log("headers: ", res.headers);
-    
-        res.on('data', function(d) {
-            console.log('Data' + d.toString());
-        });
-    });
-    
-    request.end();
-    request.on('error', function(e) {
-        console.error(e);
+    db.get(req.body.username, function(err, user) {
+	console.log(req.body);
+	if(err) console.log(err);
+	console.log(user.password);
+	if(user.password == req.body.password) {
+		req.session.user = req.body.username;
+		req.session.auth = true;
+	}
     });
     
     res.end();
 });
 
-
-app.listen(process.env.C9_PORT, '0.0.0.0');
+var port = 8080; // process.env.C9_PORT,
+var url = 'localhost'; //  '0.0.0.0'
+app.listen(port,url);
 console.log('Express server started on port %s', app.address().port);
+
+//'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+//    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+//    return v.toString(16);
+//});
